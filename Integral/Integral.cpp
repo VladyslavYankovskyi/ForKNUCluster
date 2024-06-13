29bd78c890bf0299f617f7bfb22f7b7a718cf8d5
VladyslavYankovskyi <vladislav221b@gmail.com>
2024-06-13 20:55:39 +0300

    Update

--- Integral/Integral.cpp
+++ Integral/Integral.cpp
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 
-// Функція для інтегрування
+// Function for integration
 double f(double x) {
     return sin(x) * exp(-x * x) + cos(x * x) * log(x + 1);
 }
 
-// Функція для розрахунку інтегралу методом трапецій
+// Function for calculating integral by Trepezioidal method for each processor
 double trapezoidal_method(double local_a, double local_b, int local_n, double h) {
     double integral;
     double x;
@@ -19,12 +19,13 @@ double trapezoidal_method(double local_a, double local_b, int local_n, double h)
         x = x + h;
         integral = integral + f(x);
     }
+
     integral = integral * h;
     return integral;
 }
 
 int main(int argc, char** argv) {
-    
+
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -35,15 +36,16 @@ int main(int argc, char** argv) {
     double local_int, total_int;
     double start_time, end_time, parallel_time;
 
-    double h = (b - a) / n;  // крок
-    int local_n = n / size;  // кількість інтервалів для кожного процесу
+    double h = (b - a) / n;  // step
+    int local_n = n / size;  // number of intervals for each threds
 
     double local_a = a + rank * local_n * h;
     double local_b = local_a + local_n * h;
 
-    // Вимірювання часу для паралельного виконання
+    // Calculation of programme execution time
+
     start_time = MPI_Wtime();
-    
+
     local_int = trapezoidal_method(local_a, local_b, local_n, h);
 
     MPI_Reduce(&local_int, &total_int, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -52,10 +54,11 @@ int main(int argc, char** argv) {
     parallel_time = end_time - start_time;
 
     if (rank == 0) {
-        printf("Значення інтегралу від %f до %f дорівнює %f\n", a, b, total_int);
-        printf("Час виконання паралельної програми: %f секунд\n", parallel_time);
+        printf("Integral from %f to %f is %f\n", a, b, total_int);
+        printf("Program time: %f sec\n", parallel_time);
     }
 
     MPI_Finalize();
     return 0;
+
 }
